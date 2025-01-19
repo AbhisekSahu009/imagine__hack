d9fca67e2d60d383ae06cc65ed72a921d29bf641
AbhisekSahu009 <abhi2004sahu@gmail.com>
2025-01-19 06:31:29 +0530

    first page done

--- src/components/ShareThoughts.jsx
+++ src/components/ShareThoughts.jsx
@@ -18,19 +18,43 @@ const ShareThoughts = () => {
     }, []);
 
     return (
-        <div className={`input-container ${isFocused ? 'focused' : ''}`}>
+        <div className={`share-container ${isFocused ? 'focused' : ''}`}>
             <div>
-                <h1 style={{color: '#1E90FF'}}>Hi! Akash</h1>
-                <h2 style={{color: '#C71585'}}>
+                <h1
+                    style={{
+                        backgroundImage: 'linear-gradient(90deg, #0042D4, #7620EA, #E600FF)',
+                        WebkitBackgroundClip: 'text',
+                        WebkitTextFillColor: 'transparent',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    Hi! Akash
+                </h1>
+                <h2
+                    style={{
+                        backgroundImage: 'linear-gradient(90deg, #0042D4, #7620EA, #E600FF)',
+                        WebkitBackgroundClip: 'text',
+                        WebkitTextFillColor: 'transparent',
+                        fontWeight: 'bold',
+                    }}
+                >
                     Share your thoughts
                 </h2>
-                <h2 style={{color: '#C71585'}}>
-                     <span>{text}</span>
+                <h2
+                    style={{
+                        backgroundImage: 'linear-gradient(90deg, #0042D4, #7620EA, #E600FF)',
+                        WebkitBackgroundClip: 'text',
+                        WebkitTextFillColor: 'transparent',
+                        fontWeight: 'bold',
+                        marginBottom: '20%' ,
+                    }}
+                >
+                    <span>{text}</span>
                 </h2>
-
             </div>
         </div>
     );
+
 };
 
 export default ShareThoughts;
